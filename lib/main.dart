a2e49ef3565be86141bd50592a1ec6b9e07d0485
Basith P <basithpofficial@gmail.com>
2023-10-27 16:31:39 +0530

    feat: add main dashboard layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,20 +17,53 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Emplman")),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Employees",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Managers",
-            ),
-          ],
-        ),
+      home: const MainLayout(),
+    );
+  }
+}
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Emplman")),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [
+          Center(child: Text("Employees")),
+          Center(child: Text("Eligible")),
+          Center(child: Text("Managers")),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            label: "Employees",
+            icon: Icon(FluentIcons.people_team_24_regular),
+            activeIcon: Icon(FluentIcons.people_team_24_filled),
+          ),
+          BottomNavigationBarItem(
+            label: "Eligible",
+            icon: Icon(FluentIcons.person_add_24_regular),
+            activeIcon: Icon(FluentIcons.person_add_24_filled),
+          ),
+          BottomNavigationBarItem(
+            label: "Managers",
+            icon: Icon(FluentIcons.person_24_regular),
+            activeIcon: Icon(FluentIcons.person_24_filled),
+          ),
+        ],
       ),
     );
   }
