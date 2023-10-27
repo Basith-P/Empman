89b355f37259153d235f07bf48ae4ddae3583d12
Basith P <basithpofficial@gmail.com>
2023-10-27 18:23:50 +0530

    feat: udpate bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:emplman/views/pages/main_layout.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,55 +16,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       home: const MainLayout(),
-    );
-  }
-}
-
-class MainLayout extends StatefulWidget {
-  const MainLayout({super.key});
-
-  @override
-  State<MainLayout> createState() => _MainLayoutState();
-}
-
-class _MainLayoutState extends State<MainLayout> {
-  int _currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Emplman")),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: const [
-          Center(child: Text("Employees")),
-          Center(child: Text("Eligible")),
-          Center(child: Text("Managers")),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(
-            label: "Employees",
-            icon: Icon(FluentIcons.people_team_24_regular),
-            activeIcon: Icon(FluentIcons.people_team_24_filled),
-          ),
-          BottomNavigationBarItem(
-            label: "Eligible",
-            icon: Icon(FluentIcons.person_add_24_regular),
-            activeIcon: Icon(FluentIcons.person_add_24_filled),
-          ),
-          BottomNavigationBarItem(
-            label: "Managers",
-            icon: Icon(FluentIcons.person_24_regular),
-            activeIcon: Icon(FluentIcons.person_24_filled),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/views/pages/main_layout.dart
@@ -0,0 +1,62 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:sliding_clipped_nav_bar/sliding_clipped_nav_bar.dart';
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Emplman")),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [
+          Center(child: Text("Employees")),
+          Center(child: Text("Eligible")),
+          Center(child: Text("Managers")),
+        ],
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   currentIndex: _currentIndex,
+      //   onTap: (index) => setState(() => _currentIndex = index),
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       label: "Employees",
+      //       icon: Icon(FluentIcons.people_team_24_regular),
+      //       activeIcon: Icon(FluentIcons.people_team_24_filled),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "Eligible",
+      //       icon: Icon(FluentIcons.person_add_24_regular),
+      //       activeIcon: Icon(FluentIcons.person_add_24_filled),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "Managers",
+      //       icon: Icon(FluentIcons.person_24_regular),
+      //       activeIcon: Icon(FluentIcons.person_24_filled),
+      //     ),
+      //   ],
+      // ),
+      bottomNavigationBar: SlidingClippedNavBar(
+        selectedIndex: _currentIndex,
+        onButtonPressed: (index) => setState(() => _currentIndex = index),
+        activeColor: Theme.of(context).colorScheme.primary,
+        inactiveColor: Theme.of(context).colorScheme.onSurface,
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        barItems: [
+          BarItem(icon: FluentIcons.people_team_24_regular, title: "Employees"),
+          BarItem(icon: FluentIcons.person_add_24_regular, title: "Eligible"),
+          BarItem(icon: FluentIcons.grid_24_regular, title: "Departments"),
+        ],
+      ),
+    );
+  }
+}
